d19a4b5233511f8f8cd47f6a2b3f100134e095f9
YujinOk <yujinok63@gmail.com>
2024-08-02 10:11:28 +1000

    in progress

---
+++ src/challenge.ts
@@ -0,0 +1,98 @@
+// Array Challenge
+
+const numArr: number[] = [];
+numArr.push(1);
+
+const strArr: string[] = [];
+strArr.push('Yujin');
+
+const unionArr: (number | string)[] = [];
+unionArr.push(1, 'hello', 2);
+
+// Object Challenge
+type Object = {
+  brand: string;
+  year: number;
+};
+let bike: Object = { brand: 'yamaha', year: 2000 };
+let laptop: Object = { brand: 'Apple', year: 2024 };
+let arr: Object[] = [{ brand: 'samsung', year: 2000 }];
+arr.push({ brand: 'Howgrow', year: 2020 });
+console.log(arr);
+
+const myCoworkers: string[] = ['Yujin', 'Zena', 'El', 'Blue'];
+
+const findMyCoworker = (name: string): boolean => {
+  return myCoworkers.some((cur) => cur === name);
+};
+// console.log(findMyCoworker('Yujin'));
+
+const processInput = (arg: string | number) => {
+  if (typeof arg === 'number') {
+    return arg * 2;
+  } else if (typeof arg === 'string') {
+    return arg.toUpperCase();
+  }
+};
+// console.log(processInput('yujin'));
+
+const processData = (
+  input: string | number,
+  config: { reverse: boolean } = { reverse: false }
+) => {
+  if (config.reverse && typeof input === 'string') {
+    const split = input.split('');
+    const reverse = split.reverse().join('');
+    return reverse;
+  } else if (typeof input === 'number') {
+    return Math.sqrt(input);
+  } else if (typeof input === 'string') {
+    return input.toUpperCase();
+  }
+};
+// console.log(processData(3, { reverse: true }));
+
+type Employee = {
+  id: string;
+  name: string;
+  deparment: string;
+};
+
+type Manager = {
+  id: string;
+  name: string;
+  employees: Employee[];
+};
+
+type Staff = Employee | Manager;
+
+const alice: Employee = {
+  id: 'alice',
+  name: 'Alice',
+  deparment: 'Melbourne',
+};
+
+const steve: Employee = {
+  id: 'steve',
+  name: 'Steve',
+  deparment: 'Sydney',
+};
+
+const bob: Manager = {
+  id: 'bob',
+  name: 'Bob',
+  employees: [alice, steve],
+};
+
+const printStaffDetails = (staff: Staff) => {
+  if ('employees' in staff) {
+    console.log(
+      `This staff is a manager and having ${staff?.employees?.length} employees`
+    );
+  } else {
+    console.log(
+      `This staff is an employee, belongs to ${staff?.deparment} deparment`
+    );
+  }
+};
+console.log(printStaffDetails(bob));
--- src/tutorial.ts
+++ src/tutorial.ts
@@ -17,23 +17,109 @@ const logicalFunc = (arg1: NumVariable, arg2: NumVariable): BooleanVariable => {
 // union
 let tax: number | string = 10;
 tax = 100;
-tax = "$10";
+tax = '$10';
 
-let requestStatus: "pending" | "success" | "error" = "pending";
-requestStatus = "error";
+let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+requestStatus = 'error';
 
 let notSure: any = 4;
 
 type StrArr = string[];
-const books: StrArr = ["1984", "Brave New World", "Fahrenheit 451"];
+const books: StrArr = ['1984', 'Brave New World', 'Fahrenheit 451'];
 
-let foundBook: string;
+let foundBook: string | undefined;
 
 for (let book of books) {
-  if (book === "1984") {
+  if (book === '1984') {
     foundBook = book;
-    foundBook.length;
+    foundBook = foundBook.toUpperCase();
     break;
   }
 }
-foundBook;
+console.log(foundBook?.length);
+
+// Object
+let car: { brand: string; year: number } = { brand: 'toyota', year: 2020 };
+
+// Function
+function sayHi(name: string) {
+  console.log(`Hello there ${name.toUpperCase()}`);
+}
+sayHi('yujin');
+
+function calculateDiscount(price: number): number {
+  return price * 0.9;
+}
+
+const finalPrice = calculateDiscount(200);
+
+function calculatePrice(price: number, discount?: number): number {
+  return price - (discount || 0);
+}
+
+let priceAfterDiscount = calculatePrice(100, 20);
+
+function sum(message: string, ...numbers: number[]): string {
+  const doubled = numbers.map((num) => {
+    console.log(num);
+    return num * 2;
+  });
+  console.log(doubled);
+  let total = numbers.reduce((previous, current) => {
+    console.log({ previous, current });
+    return previous + current;
+  }, 0);
+  return `${message} ${total}`;
+}
+
+let result = sum('The total is: ', 1, 2, 3, 4, 5);
+console.log(result);
+
+const createEmployee = ({
+  id,
+}: {
+  id: number;
+}): { id: number; isActive: boolean } => {
+  return { id, isActive: id % 2 === 0 };
+};
+
+const first = createEmployee({ id: 1 });
+const second = createEmployee({ id: 2 });
+console.log({ first, second });
+
+const createStudent = (student: { id: number; name: string }): void => {
+  console.log(`Wecome to the course ${student.name.toUpperCase()}`);
+};
+
+const newStudent = {
+  id: 5,
+  name: 'anna',
+  email: 'bobo@gmail.com',
+};
+createStudent(newStudent);
+
+type User = {
+  id: number;
+  name: string;
+  isActive: boolean;
+};
+const john: User = {
+  id: 1,
+  name: 'john',
+  isActive: true,
+};
+const susan: User = {
+  id: 1,
+  name: 'susan',
+  isActive: false,
+};
+
+function createUser(user: User): {
+  id: number;
+  name: string;
+  isActive: boolean;
+} {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+
+  return user;
+}
